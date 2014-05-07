11d3aa15bcc16826ee70de16f32a33fb47c5acba
Thomas Uster <thomasuster@gmail.com>
2014-05-07 15:57:39 -0700

    Test fix.

--- test/openfl/display/DynamicTilesheetTest.hx
+++ test/openfl/display/DynamicTilesheetTest.hx
@@ -9,28 +9,45 @@ class DynamicTilesheetTest {
 
     var inImage:BitmapData;
     var tileSheet:Tilesheet;
+    var sprite:Sprite;
+
+    @Setup
+    public function setupRedPixel():Void {
+        inImage = new BitmapData(1,1, false, 0xFFFF0000);
+        tileSheet = new Tilesheet(inImage);
+        var rect:Rectangle = new Rectangle(0,0,1,1);
+        tileSheet.addTileRect(rect);
+        sprite = new Sprite();
+        draw();
+    }
 
 	@Test
 	public function establishRedPixel():Void {
         setupRedPixel();
+        draw();
         MatcherAssert.assertThat(tileSheet.__bitmap.getPixel(0,0), IsEqual.equalTo(0xFF0000));
 	}
 
     @Test
-    public function testSetBitmapData():Void {
+    public function testGreenPixelBeforeDraw():Void {
         setupRedPixel();
         inImage.setPixel(0,0, 0x00FF00);
+        draw();
         MatcherAssert.assertThat(tileSheet.__bitmap.getPixel(0,0), IsEqual.equalTo(0x00FF00));
     }
 
-    function setupRedPixel():Void {
-        inImage = new BitmapData(1,1, true, 0xFFFF0000);
-        tileSheet = new Tilesheet(inImage);
-        var rect:Rectangle = new Rectangle(0,0,1,1);
-        tileSheet.addTileRect(rect);
-        var sprite = new Sprite();
+    @Test
+    public function testGreenPixelAfterDraw():Void {
+        setupRedPixel();
+        draw();
+        inImage.setPixel(0,0, 0x00FF00);
+        draw();
+        MatcherAssert.assertThat(tileSheet.__bitmap.getPixel(0,0), IsEqual.equalTo(0x00FF00));
+    }
+
+
+
+    function draw():Void {
         tileSheet.drawTiles(sprite.graphics, [0]);
     }
-	
-	
 }
