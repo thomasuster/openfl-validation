a43d7f63b81fc8cc5c7e09856c78ecfe868496b2
Thomas Uster <thomasuster@gmail.com>
2014-05-07 16:52:15 -0700

    Draw from TileSheet problem

--- test/openfl/display/DynamicTilesheetTest.hx
+++ test/openfl/display/DynamicTilesheetTest.hx
@@ -10,6 +10,7 @@ class DynamicTilesheetTest {
     var inImage:BitmapData;
     var tileSheet:Tilesheet;
     var sprite:Sprite;
+    var output:BitmapData;
 
     @Setup
     public function setupRedPixel():Void {
@@ -21,33 +22,40 @@ class DynamicTilesheetTest {
         draw();
     }
 
+    @Test
+    public function establishRedPixelWithTilesBitmapData():Void {
+        setupRedPixel();
+        draw();
+        MatcherAssert.assertThat(tileSheet.__bitmap.getPixel(0,0), IsEqual.equalTo(0xFF0000));
+    }
+
 	@Test
 	public function establishRedPixel():Void {
         setupRedPixel();
         draw();
-        MatcherAssert.assertThat(tileSheet.__bitmap.getPixel(0,0), IsEqual.equalTo(0xFF0000));
+        MatcherAssert.assertThat(output.getPixel(0,0), IsEqual.equalTo(0xFF0000));
 	}
 
-    @Test
-    public function testGreenPixelBeforeDraw():Void {
-        setupRedPixel();
-        inImage.setPixel(0,0, 0x00FF00);
-        draw();
-        MatcherAssert.assertThat(tileSheet.__bitmap.getPixel(0,0), IsEqual.equalTo(0x00FF00));
-    }
-
-    @Test
-    public function testGreenPixelAfterDraw():Void {
-        setupRedPixel();
-        draw();
-        inImage.setPixel(0,0, 0x00FF00);
-        draw();
-        MatcherAssert.assertThat(tileSheet.__bitmap.getPixel(0,0), IsEqual.equalTo(0x00FF00));
-    }
-
-
+//    @Test
+//    public function testGreenPixelBeforeDraw():Void {
+//        setupRedPixel();
+//        inImage.setPixel(0,0, 0x00FF00);
+//        draw();
+//        MatcherAssert.assertThat(output.getPixel(0,0), IsEqual.equalTo(0x00FF00));
+//    }
+//
+//    @Test
+//    public function testGreenPixelAfterDraw():Void {
+//        setupRedPixel();
+//        draw();
+//        inImage.setPixel(0,0, 0x00FF00);
+//        draw();
+//        MatcherAssert.assertThat(output.getPixel(0,0), IsEqual.equalTo(0x00FF00));
+//    }
 
     function draw():Void {
         tileSheet.drawTiles(sprite.graphics, [0]);
+        output = new BitmapData(1,1);
+        output.draw(sprite);
     }
 }
