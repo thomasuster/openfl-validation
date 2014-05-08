8a785dea85858911492e1f0612409e6448d5b1b1
Thomas Uster <thomasuster@gmail.com>
2014-05-08 12:17:07 -0700

    possible test

--- test/openfl/display/DynamicTilesheetTest.hx
+++ test/openfl/display/DynamicTilesheetTest.hx
@@ -1,10 +1,13 @@
 package openfl.display;
+import flash.Lib;
+import flash.utils.ByteArray;
 import flash.display.Sprite;
 import org.hamcrest.core.IsEqual;
 import org.hamcrest.MatcherAssert;
 import flash.geom.Rectangle;
 import openfl.display.Tilesheet;
 import flash.display.BitmapData;
+import openfl.gl.GL;
 class DynamicTilesheetTest {
 
     var inImage:BitmapData;
@@ -14,20 +17,32 @@ class DynamicTilesheetTest {
 
     @Setup
     public function setupRedPixel():Void {
-        inImage = new BitmapData(1,1, false, 0xFFFF0000);
+        sprite = new Sprite();
+        Lib.current.stage.addChild(sprite);
+        inImage = new BitmapData(1,1, true, 0xFFFF0000);
         tileSheet = new Tilesheet(inImage);
         var rect:Rectangle = new Rectangle(0,0,1,1);
         tileSheet.addTileRect(rect);
-        sprite = new Sprite();
         draw();
     }
 
-    @Test
-    public function establishRedPixelWithTilesBitmapData():Void {
-        setupRedPixel();
-        draw();
-        MatcherAssert.assertThat(tileSheet.__bitmap.getPixel(0,0), IsEqual.equalTo(0xFF0000));
-    }
+//    @Test
+//    public function establishRedPixelWithTilesBitmapData():Void {
+//        setupRedPixel();
+//        draw();
+////        var pixels:ByteArray = new ByteArray();
+////        pixels.writeInt(5);
+////        pixels.position = 0;
+////        trace(pixels.readInt());
+////        GL.clear(0);
+//        var pixels:ByteArray = new ByteArray();
+//        pixels.writeInt(0);
+//        pixels.position = 0;
+//        GL.readPixels(0,0,1,1,GL.RGBA, GL.INT, pixels);
+//        pixels.position = 0;
+//        trace(pixels.readInt());
+////        MatcherAssert.assertThat(pixels.readInt(), IsEqual.equalTo(0xFF0000));
+//
 
 	@Test
 	public function establishRedPixel():Void {
@@ -36,13 +51,13 @@ class DynamicTilesheetTest {
         MatcherAssert.assertThat(output.getPixel(0,0), IsEqual.equalTo(0xFF0000));
 	}
 
-//    @Test
-//    public function testGreenPixelBeforeDraw():Void {
-//        setupRedPixel();
-//        inImage.setPixel(0,0, 0x00FF00);
-//        draw();
-//        MatcherAssert.assertThat(output.getPixel(0,0), IsEqual.equalTo(0x00FF00));
-//    }
+    @Test
+    public function testGreenPixelBeforeDraw():Void {
+        setupRedPixel();
+        inImage.setPixel(0,0, 0x00FF00);
+        draw();
+        MatcherAssert.assertThat(output.getPixel(0,0), IsEqual.equalTo(0x00FF00));
+    }
 //
 //    @Test
 //    public function testGreenPixelAfterDraw():Void {
@@ -54,7 +69,7 @@ class DynamicTilesheetTest {
 //    }
 
     function draw():Void {
-        tileSheet.drawTiles(sprite.graphics, [0]);
+        tileSheet.drawTiles(sprite.graphics, [0,0,0]);
         output = new BitmapData(1,1);
         output.draw(sprite);
     }
